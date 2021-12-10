f829740eb55e3be90eb542a73befc89d52cb9f39
unknown <navaneethsharma2310oct@gmail.com>
2021-12-10 23:15:47 +0530

    django inital

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+import home
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('resetpassword', views.resetpassword, name='resetpassword'),
+    path('forgotpassword', views.forgotpassword, name='forgotpassword'),
+    
+]
---
+++ home/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def resetpassword(request):
+    return render(request, 'resetpassword.html')
+
+def forgotpassword(request):
+    return render(request, 'forgotpassword.html')
+
+
+
+
+# <!-- <button>Sign in</button>
+#                         <p style="margin-right: -300px;"><a href="{{url_for('forgotpassword')}}" style=" color: #d4dce1;;">Forgot
+#                                 password?</a></p>
+                                
+#                             <h5>
+#                         <h5>
+#                             <span style="color: #d4dce1;">Don't have an account?</span>
+#                             <b> <a href="{{url_for('signup')}}" style="color: #ec4141" class="pointer">Sign up</a></b>
+
+#                         </h5> -->
+
+#                         <!-- {% with messages = get_flashed_messages() %}
+#                         {% if messages %}
+#                                 {% for message in messages %}
+#                                 <div class="alert alert-warning alert-dismissible" style="color: green;">
+                                
+#                                           {{ message }}
+#                                         </div>
+#                                 {% endfor %}
+#                         {% endif %}
+#                     {% endwith %} -->
