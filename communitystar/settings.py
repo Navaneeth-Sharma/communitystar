42471ee9167864ab853002f2eb09620df42a28b1
VRUJESH H M <60779508+VRU2306@users.noreply.github.com>
2021-12-12 23:28:57 +0530

    comiit

--- communitystar/settings.py
+++ communitystar/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
