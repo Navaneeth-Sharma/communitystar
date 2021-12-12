59e18eba3e618106ea8250f9ae70b186ffd5e350
unknown <navaneethsharma2310oct@gmail.com>
2021-12-12 20:42:19 +0530

    Heroku Deployment codes-2.8

--- communitystar/settings.py
+++ communitystar/settings.py
@@ -169,7 +169,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(os.path.dirname( __file__ ), '..', 'static'),
 )
 
 # PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
