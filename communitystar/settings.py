f85aa398049bfdd79a9b1c63a7d013fb64b7eebf
VRUJESH H M <60779508+VRU2306@users.noreply.github.com>
2021-12-12 23:51:20 +0530

    Update settings.py

--- communitystar/settings.py
+++ communitystar/settings.py
@@ -21,7 +21,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'static/'),
 )
 
 # Quick-start development settings - unsuitable for production
