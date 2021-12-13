de54dd52077d73c5762d120de17916cc2e37f01a
unknown <navaneethsharma2310oct@gmail.com>
2021-12-13 11:18:20 +0530

    Heroku Added 7

--- communitystar/settings.py
+++ communitystar/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, './static/')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
