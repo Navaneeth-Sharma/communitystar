487dda6000128496a4a11b481e10685a2b04becf
unknown <navaneethsharma2310oct@gmail.com>
2021-12-13 11:29:01 +0530

    Heroku Added 10

--- communitystar/settings.py
+++ communitystar/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, './home/static/'),
+    os.path.join(BASE_DIR, './home/static/assets'),
 )
 
 
