2e9f8149cf40c7924aec82844293c66fb1379f8a
unknown <navaneethsharma2310oct@gmail.com>
2021-12-13 17:15:59 +0530

    Heroku Deployment t:2.2

--- communitystar/settings.py
+++ communitystar/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware'
 ]
 
 ROOT_URLCONF = 'communitystar.urls'
