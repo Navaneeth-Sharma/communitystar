73dc1d861a830ac9f672636ce5aa498964c634c6
unknown <navaneethsharma2310oct@gmail.com>
2021-12-13 17:09:21 +0530

    Heroku Deployment t:2.1

--- communitystar/urls.py
+++ communitystar/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +23,4 @@ urlpatterns = [
     path('social-auth/', include('social_django.urls', namespace='social')),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += staticfiles_urlpatterns()
