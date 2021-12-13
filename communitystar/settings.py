375a1e7ed28edaa0c65b190d974f9ed2557652fc
unknown <navaneethsharma2310oct@gmail.com>
2021-12-13 18:07:00 +0530

    Heroku Deployment t:2.5

--- communitystar/settings.py
+++ communitystar/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, './db.sqlite3'),
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',  
     }
 }
 
