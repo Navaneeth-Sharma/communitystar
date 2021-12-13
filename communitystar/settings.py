b8da0b5ee943dc5bf3357ccbc7f02bae8dd93ed9
unknown <navaneethsharma2310oct@gmail.com>
2021-12-13 12:26:11 +0530

    Heroku Added 14

--- communitystar/settings.py
+++ communitystar/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = " "
+STATIC_ROOT = "/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'./static'),
 ]
 
 # Default primary key field type
