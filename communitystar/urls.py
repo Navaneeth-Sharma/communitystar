9970adde17100d6b3ad8b5e0fa270ec5d5eeff64
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2021-12-18 19:36:54 +0530

    formatting the code

--- communitystar/urls.py
+++ communitystar/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('social-auth/', include('social_django.urls', namespace='social')),
+    path("admin/", admin.site.urls),
+    path("", include("home.urls")),
+    path("social-auth/", include("social_django.urls", namespace="social")),
 ]
